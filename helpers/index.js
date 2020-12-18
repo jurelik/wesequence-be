47bc43291b4fdddba12ac81eee266932dc2caa58
jurelik <jure@likar.me>
2020-12-18 17:46:48 +0000

    moved helper functions into a separate folder

---
+++ helpers/index.js
@@ -0,0 +1,36 @@
+const rooms = { //Global rooms object
+  test: []
+}
+
+const dbINIT = () => {
+  models.sequelize.sync().then(async () => {
+    const t = await db.transaction();
+
+    try {
+      await db.query(`INSERT INTO rooms (name, "createdAt", "updatedAt") VALUES ('test', NOW(), NOW())`, { type: Sequelize.QueryTypes.INSERT, transaction: t  })
+      await db.query(`INSERT INTO scenes (num, "createdAt", "updatedAt", "roomId") VALUES (0, NOW(), NOW(), 1)`, { type: Sequelize.QueryTypes.INSERT, transaction: t  })
+      await db.query(`INSERT INTO tracks (name, url, sequence, "createdAt", "updatedAt", "sceneId") VALUES ('kick', 'https://postead.s3.eu-west-2.amazonaws.com/kick.wav', ARRAY [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0], NOW(), NOW(), 1)`, { type: Sequelize.QueryTypes.INSERT, transaction: t  })
+      await t.commit();
+    }
+    catch (err) {
+      await t.rollback();
+      console.log(err);
+    }
+  }).catch(err => {
+    console.error(err);
+  });
+}
+
+const sendToRoom = (payload, ws) => {
+  for (const socket of rooms[ws.room]) {
+    if (socket !== ws) {
+      socket.send(JSON.stringify(payload));
+    }
+  }
+}
+
+module.exports = {
+  rooms,
+  dbINIT,
+  sendToRoom
+}
--- index.js
+++ index.js
@@ -2,49 +2,17 @@ require('dotenv-flow').config();
 const WebSocket = require('ws');
 const { Sequelize } = require('sequelize');
 const models = require('./models');
-
-const rooms = { //Global rooms object
-  test: []
-}
+const helpers = require('./helpers');
 
 const wss = new WebSocket.Server({ port: process.env.PORT  });
 const db = new Sequelize(`postgres://${process.env.DB_USER}@${process.env.DB_URL}:5432/${process.env.DB_NAME}`)
 
-//Helper functions
-const dbINIT = () => {
-  models.sequelize.sync().then(async () => {
-    const t = await db.transaction();
-
-    try {
-      await db.query(`INSERT INTO rooms (name, "createdAt", "updatedAt") VALUES ('test', NOW(), NOW())`, { type: Sequelize.QueryTypes.INSERT, transaction: t  })
-      await db.query(`INSERT INTO scenes (num, "createdAt", "updatedAt", "roomId") VALUES (0, NOW(), NOW(), 1)`, { type: Sequelize.QueryTypes.INSERT, transaction: t  })
-      await db.query(`INSERT INTO tracks (name, url, sequence, "createdAt", "updatedAt", "sceneId") VALUES ('kick', 'https://postead.s3.eu-west-2.amazonaws.com/kick.wav', ARRAY [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0], NOW(), NOW(), 1)`, { type: Sequelize.QueryTypes.INSERT, transaction: t  })
-      await t.commit();
-    }
-    catch (err) {
-      await t.rollback();
-      console.log(err);
-    }
-  }).catch(err => {
-    console.error(err);
-  });
-
-}
-
-const sendToRoom = (payload, ws) => {
-  for (const socket of rooms[ws.room]) {
-    if (socket !== ws) {
-      socket.send(JSON.stringify(payload));
-    }
-  }
-}
-
-dbINIT(); //Uncomment only when initialising db
+//helpers.dbINIT(); //Uncomment only when initialising db
 
 wss.on('connection', async (ws, req) => {
   if (req.url === '/test') {
     // Add client to room & room to client
-    rooms.test.push(ws);
+    helpers.rooms.test.push(ws);
     ws.room = req.url.substr(1);
 
     ws.send(JSON.stringify({
@@ -72,14 +40,14 @@ wss.on('connection', async (ws, req) => {
 
     switch (data.type) {
       case 'SEQ_BUTTON_PRESS':
-        sendToRoom({
+        helpers.sendToRoom({
           type: 'SEQ_BUTTON_PRESS',
           trackName: data.trackName,
           position: data.position
         }, ws);
         break;
       case 'CHANGE_TEMPO':
-        sendToRoom({
+        helpers.sendToRoom({
           type: 'CHANGE_TEMPO',
           tempo: data.tempo
         }, ws);
@@ -90,7 +58,7 @@ wss.on('connection', async (ws, req) => {
   });
 
   ws.on('close', () => {
-    rooms.test.splice(rooms.test.indexOf(ws), 1);
+    helpers.rooms.test.splice(helpers.rooms.test.indexOf(ws), 1);
   })
 });
 
